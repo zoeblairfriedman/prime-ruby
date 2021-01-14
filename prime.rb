290d1ea3a1fb6961026a7b6c3152383be1c5627c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-14 10:46:01 -0500

    commit tree 9b2244912ee923d06c805c13d6bb897d677ad996

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+range = 2..num-1
+
+if num == 2
+    true
+elsif num == 1
+    false
+elsif num <= 0
+    false
+else
+range.all? do |n|
+  num % n != 0    
+  end
+
+end
+
+end
+
+# prime?(-1)
